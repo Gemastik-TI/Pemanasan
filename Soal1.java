dae2200fe5f0dafe63f6c1115735864fad15fcab
Ibnufadillah <68468056+Ibnufadillah@users.noreply.github.com>
2020-07-18 20:44:52 +0800

    Add files via upload

---
+++ Soal1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soal1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ibnu Fadillah
+ */
+public class Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        int X = in.nextInt();
+        int Y = in.nextInt();
+        int Z = in.nextInt();
+        
+        if ((X+Y>Z)&&(X+Z>Y)&&(Y+Z>X)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+    
+}
